6c351c46cef7111f3d02b11baacb2fd097502d54
Muhammad Razif <razifnations@gmail.com>
2023-06-13 21:18:05 +0700

    LatihanBuku

---
+++ PBO_2201082039/src/Muhammad_Razif_Buku_2039_13062023/Buku.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Muhammad_Razif_Buku_2039_13062023;
+
+/**
+ *
+ * @author ACER
+ */
+public class Buku {
+    private String kodeBuku;    
+    private String judulBuku;
+    private String pengarang;
+    private String tahunTerbit;
+    
+    public String getKodeBuku() {
+        return kodeBuku;
+    }
+
+    public void setKodeBuku(String kodeBuku) {
+        this.kodeBuku = kodeBuku;
+    }
+
+    public String getJudulBuku() {
+        return judulBuku;
+    }
+
+    public void setJudulBuku(String judulBuku) {
+        this.judulBuku = judulBuku;
+    }
+
+    public String getPengarang() {
+        return pengarang;
+    }
+
+    public void setPengarang(String pengarang) {
+        this.pengarang = pengarang;
+    }
+
+    public String getTahunTerbit() {
+        return tahunTerbit;
+    }
+
+    public void setTahunTerbit(String tahunTerbit) {
+        this.tahunTerbit = tahunTerbit;
+    }
+}
